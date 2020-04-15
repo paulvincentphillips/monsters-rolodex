d9e27d2fd5b1e2a900759349540da0f99d51f1b3
paulvincentphillips@gmail.com <paulvincentphillips@gmail.com>
2020-04-15 12:10:54 +0100

    Updates

---
+++ precache-manifest.85b95c00911792404bd305603ba10a68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a85042d9d4cafc5e188e04c6344527",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "64fcb3af60cb09f9a3c1",
+    "url": "/monsters-rolodex/static/css/main.e9afd808.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64fcb3af60cb09f9a3c1",
+    "url": "/monsters-rolodex/static/js/main.e43a90bd.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
